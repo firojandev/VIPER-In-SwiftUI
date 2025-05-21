f4b8f25fabbbcc045dbe15afc2ac5daaebc9efb0
Md Altaf Hoshain <altaf@squaregroup.com>
2025-05-21 16:40:10 +0600

    Initial Commit

---
+++ SwiftUI VIPER/SwiftUI_VIPERApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_VIPERApp.swift
+//  SwiftUI VIPER
+//
+//  Created by Md Altaf Hoshain Firoj on 21/5/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_VIPERApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
